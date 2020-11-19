21d02075a52477f7052763fae538cfb0b0d54304
Dejan Toteff <deyan8284@gmail.com>
2020-11-20 01:57:37 +0200

    chore@dnd

--- src/_modules/parse-challenge-data.spec.js
+++ src/_modules/parse-challenge-data.spec.js
@@ -1,8 +1,11 @@
 import { parseChallengeData } from './parse-challenge-data'
 import {data} from '../challange-test-data.json'
+import { writeJson } from 'fs-extra'
 
-test('happy', () => {
+test('happy', async () => {
   const result = parseChallengeData(data)
+  await writeJson(`${__dirname}/parsed-test-data.json`, {data:result})
+
   expect(
     result
   ).toMatchSnapshot()
